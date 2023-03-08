c6ec1af522cee28974fcde569ad0adf4b18847f9
universuccess <cong@wee.vn>
2023-03-08 17:43:58 +0700

    add router

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
+import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom'
 import App from './App'
 import './index.css'
 import store from './redux'
 
+const router = createBrowserRouter([
+  {
+    index: true,
+    element: <App />
+  },
+  {
+    path: '*',
+    element: <Navigate to="/" replace />
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <App />
+    <RouterProvider router={router} />
   </Provider>,
 )
